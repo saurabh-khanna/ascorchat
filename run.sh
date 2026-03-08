6ee0e47a6e3878d8a201e113dd3e60c82afb93cf
Saurabh Khanna <saurabh.khanna@uva.nl>
2026-03-08 16:00:16 +0100

    Update run.sh script comments

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 streamlit run --browser.serverAddress $SERVER_NAME --server.port $PORT app.py
 
 # #!/usr/bin/env bash
-# # Convenience script - runs surveychat locally.
-# # Usage: sh run.sh
+# # Convenience script for testing on AWS EC2 or similar platforms, where you may not have Streamlit installed globally and/or want to specify the server address and port via environment variables.
 
 # streamlit run app.py
